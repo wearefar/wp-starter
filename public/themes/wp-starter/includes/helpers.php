3b0a24fa426c7fa32db5264cbfce3d4cd01eac5f
Victor Guerrero <vguerrerobosch@users.noreply.github.com>
2022-05-25 17:37:31 +0200

    Add helper widont

--- public/themes/wp-starter/includes/helpers.php
+++ public/themes/wp-starter/includes/helpers.php
@@ -75,6 +75,20 @@ if (!function_exists('svg')) {
     }
 }
 
+if (! function_exists('widont')) {
+    /**
+     * @see https://shauninman.com/archive/2007/01/03/widont_2_1_wordpress_plugin
+     */
+    function widont($string, $value = 2)
+    {
+        if ($value > 2) {
+            $string = widont($string, $value - 1);
+        }
+
+        return preg_replace('/([^\s])\s+([^\s]+)\s*$/', '$1&nbsp;$2', rtrim($string));
+    }
+}
+
 if (! function_exists('asset')) {
     function asset(string $path)
     {
